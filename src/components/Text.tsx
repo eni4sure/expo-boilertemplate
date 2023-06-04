4447018dec3ea4a44ede0ffcef9fb40eb2d74f98
Eniola Osabiya <eosabiya@gmail.com>
2023-06-04 09:47:43 -0500

    chore: reset predefined text props, not good

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -9,41 +9,6 @@ export default function CustomText({ children, ...props }: CustomTextProp) {
         <Text
             // breaker
             {...props}
-            h1Style={[
-                props.h1Style,
-                {
-                    // other default css here to match design theme
-                    fontSize: 32,
-                },
-            ]}
-            h2Style={[
-                props.h2Style,
-                {
-                    // other default css here to match design theme
-                    fontSize: 24,
-                },
-            ]}
-            h3Style={[
-                props.h3Style,
-                {
-                    // other default css here to match design theme
-                    fontSize: 20,
-                },
-            ]}
-            h4Style={[
-                props.h4Style,
-                {
-                    // other default css here to match design theme
-                    fontSize: 16,
-                },
-            ]}
-            style={[
-                props.style,
-                {
-                    // other default css here to match design theme
-                    fontSize: 14,
-                },
-            ]}
         >
             {children}
         </Text>
